d551bcb13bc94e08845240dba686370428a445db
Aleksei <me@shbbl.dev>
2023-03-30 15:36:42 +0400

    lint stuff

--- internal/tghandler/utils.go
+++ internal/tghandler/utils.go
@@ -1,8 +1,9 @@
 package tghandler
 
 import (
+	"crypto/rand"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
-	"math/rand"
+	"math/big"
 	"strings"
 	"time"
 )
@@ -14,9 +15,11 @@ type ChatCache struct {
 var cache = make(map[int64]ChatCache)
 
 func isItTime(chat int64) bool {
-	s := rand.NewSource(time.Now().Unix())
-	r := rand.New(s) // initialize local pseudorandom generator
-	n := r.Intn(100)
+	nBig, err := rand.Int(rand.Reader, big.NewInt(100))
+	if err != nil {
+		panic(err)
+	}
+	n := nBig.Int64()
 
 	if n > 95 && time.Since(cache[chat].lastRand) > 10*time.Minute {
 		newCache := ChatCache{time.Now()}
